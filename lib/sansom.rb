14fdcbf209af61f6f5e8455a2e7684e41f8a2533
Nathaniel Symer <nate@natesymer.com>
2014-08-07 23:25:22 -0400

    Error handler.

--- lib/sansom.rb
+++ lib/sansom.rb
@@ -7,6 +7,7 @@ require_relative "./rack/fastlint.rb"
 module Sansomable
   InvalidRouteError = Class.new StandardError
   HTTP_VERBS = [:get,:head, :post, :put, :delete, :patch, :options, :link, :unlink, :trace].freeze
+  ROUTE_METHODS = HTTP_VERBS+[:map]
   RACK_HANDLERS = ["puma", "unicorn", "thin", "webrick"].freeze
   NOT_FOUND = [404, {}, ["Not found."]].freeze
 
@@ -20,45 +21,54 @@ module Sansomable
   
   def call env
     return NOT_FOUND if tree.leaf? && tree.root?
-    
+  
     r = Rack::Request.new env
     m = tree.match r.path_info, r.request_method
-    
+  
     return NOT_FOUND if m.nil?
     
-    if @before_block && @before_block.arity == 1
-      bres = @before_block.call r
-      return bres if Rack::Fastlint.response bres
-    end
+    begin
+      if @before_block && @before_block.arity == 1
+        bres = @before_block.call r
+        return bres if Rack::Fastlint.response bres
+      end
 
-    if m.url_params.count > 0
-      q = r.params.merge m.url_params
-      s = q.map { |p| p.join '=' }.join '&'
-      r.env["rack.request.query_hash"] = q
-      r.env["rack.request.query_string"] = s
-      r.env["QUERY_STRING"] = s
-      r.instance_variable_set "@params", r.POST.merge(q)
-    end
+      if m.url_params.count > 0
+        q = r.params.merge m.url_params
+        s = q.map { |p| p.join '=' }.join '&'
+        r.env["rack.request.query_hash"] = q
+        r.env["rack.request.query_string"] = s
+        r.env["QUERY_STRING"] = s
+        r.instance_variable_set "@params", r.POST.merge(q)
+      end
     
-    case m.item
-    when Proc then res = m.item.call r
-    else
-      raise InvalidRouteError, "Route handlers must be blocks or valid rack apps." unless m.item.respond_to? :call
-      r.env["PATH_INFO"] = m.remaining_path
-      res = m.item.call r.env
-    end
+      case m.item
+      when Proc then res = m.item.call r
+      else
+        raise InvalidRouteError, "Route handlers must be blocks or valid rack apps." unless m.item.respond_to? :call
+        r.env["PATH_INFO"] = m.remaining_path
+        res = m.item.call r.env
+      end
     
-    if @after_block && @after_block.arity == 2
-      ares = @after_block.call r, res
-      return ares if Rack::Fastlint.response ares
-    end
+      if @after_block && @after_block.arity == 2
+        ares = @after_block.call r, res
+        return ares if Rack::Fastlint.response ares
+      end
     
-    res
+      res
+    rescue StandardError => e
+      raise if @error_block.nil?
+      @error_block.call e, r 
+    end
   end
   
   def start port=3001
     raise NoRoutesError if tree.leaf?
     Rack::Handler.pick(RACK_HANDLERS).run self, :Port => port
+  end
+  
+  def error &block
+    @error_block = block
   end
   
   def before &block
@@ -70,10 +80,9 @@ module Sansomable
   end
   
   def method_missing meth, *args, &block
-    path, item = *args.dup.push(block)
     return super unless path && item
     return super unless item != self
-    return super unless (HTTP_VERBS+[:map]).include?(meth)
+    return super unless ROUTE_METHODS.include? meth
     tree.map_path path, item, meth
   end
 end
